a5c595f7f0caab9ce79ba94f1e8571b8af632bd0
Jake Cannon <jakecan13@gmail.com>
2020-02-14 17:12:37 -0500

    add initial getNoun fetch call

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
-function getNoun() {
-	const myArray = ['Dog', 'Cat', 'Coffee', 'Library'];
-	let item = myArray[Math.floor(Math.random() * myArray.length)]
-	let x = document.getElementById('txt');
-	x.value = item;	
-}
+// function getNoun() {
+// 	const myArray = ['Dog', 'Cat', 'Coffee', 'Library'];
+// 	let item = myArray[Math.floor(Math.random() * myArray.length)]
+// 	let x = document.getElementById('txt');
+// 	x.value = item;	
+// }
 
 function getAdj() {
 	const myArray = ['Running', 'Jumping', 'Sleeping', 'Dieing'];
@@ -21,26 +21,26 @@ function getAdjAndNoun() {
 	x.value = item;	
 }
 
+// This getNoun function currently works but its pretty sloppy
+// could make the fetch API call more DRY
+// Also not sure th
+function getNoun() {
+	fetch('http://api.wordnik.com/v4/words.json/randomWord?api_key=4nj9t1spsnu7k02y4vhr8wct3qrqckgpyv41g252r3gy5qdqg')
+	.then(function(response) {
+	response.json().then(function(object) {
+		console.log(object.word)
+		let word = object.word
+		console.log(word)
+		// let x = document.getElementById('txt');
+		// x.value = word;
 
-// fetch() tutorial
-// https://www.youtube.com/watch?v=cuEtnrL9-H0
-//
-// Fetch is promised base
-// We can use asynch await
-// or
-// .then() and .catch()
-
-// fetch('https://reqres.in/api/users')
-// 	.then(res => {
-// 	 if (res.ok) {
-// 	 	console.log('SUCCESS')
-// 	 } else {
-// 	 	console.log('Not Successful')
-// 	 }
-// 	})
-// 	.then(data => console.log(data))
-// 	.catch(error => console.log('ERROR'))
+		// not sure what .value means here
+		document.getElementById('txt').value = word;
+	})
+})
+}
 
 
-//use this API (Note, if you don't pay 5$ it takes 7 days to recieve..)
-//https://developer.wordnik.com/gettingstarted
+
+
+
