3504192e28ad279ed848a4370f9692dd3ae9ee89
Jake Cannon <jakecan13@gmail.com>
2020-02-15 13:52:10 -0500

    add boiler plate API routes

--- app.py
+++ app.py
@@ -9,5 +9,18 @@ app = Flask(__name__)
 def index():
 	return jsonify({'message': 'TEST'}), 200
 
+@app.route('/noun')
+def get_noun():
+	pass
+
+@app.route('/adjective')
+def get_adjective():
+	pass
+
+@app.route('/adjective_and_noun')
+def get_adjective_and_noun():
+	pass
+
 if __name__ == '__main__':
+	app.debug = True
 	app.run()
